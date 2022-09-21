391067ffda1791c38654248df6a6c28a59dd6fcf
AkashOptify <akash.rane@optifysolutions.com>
2022-09-21 14:48:07 +0530

    formated

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
-//PSUDO CODE 
-//setup basic
-void setup() {
+// PSUDO CODE
+// setup basic
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
